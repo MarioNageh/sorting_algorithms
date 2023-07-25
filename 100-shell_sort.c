56ebff3b3cb875511b5bd28e47ad92d773206102
Mario Nageh <marionageh7@gmail.com>
2023-07-25 10:58:30 +0300

    Shell Sort Fix Betty

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,30 +11,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-    int gab = 1, i, j, temp;
+	int gab = 1, i, j, temp;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (gab < (int) size / 3)
-    {
-        gab = gab * 3 + 1;
-    }
+	while (gab < (int) size / 3)
+	{
+		gab = gab * 3 + 1;
+	}
 
-    while (gab >= 1)
-    {
-        for (i = gab; i < (int) size; i++)
-        {
-            temp = array[i];
-            j = i - gab;
-            while (j >= 0 && array[j] > temp)
-            {
-                array[j + gab] = array[j];
-                j = j - gab;
-            }
-            array[j + gab] = temp;
-        }
-        gab = gab / 3;
-        print_array(array, size);
-    }
+	while (gab >= 1)
+	{
+		for (i = gab; i < (int) size; i++)
+		{
+			temp = array[i];
+			j = i - gab;
+			while (j >= 0 && array[j] > temp)
+			{
+				array[j + gab] = array[j];
+				j = j - gab;
+			}
+			array[j + gab] = temp;
+		}
+		gab = gab / 3;
+		print_array(array, size);
+	}
 }
