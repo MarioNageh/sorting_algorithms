9c2446e7b8ca5cc3d94b677b05898b4e07ae226a
Mario Nageh <marionageh7@gmail.com>
2023-07-25 10:57:47 +0300

    Shell Sort Fixing

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,22 +11,30 @@
 
 void shell_sort(int *array, size_t size)
 {
-    int gab, i, j, temp;
+    int gab = 1, i, j, temp;
 
-    for (gab = size / 3; gab > 0; gab /= 3)
+    if (array == NULL || size < 2)
+        return;
+
+    while (gab < (int) size / 3)
+    {
+        gab = gab * 3 + 1;
+    }
+
+    while (gab >= 1)
     {
         for (i = gab; i < (int) size; i++)
         {
             temp = array[i];
             j = i - gab;
-
             while (j >= 0 && array[j] > temp)
             {
                 array[j + gab] = array[j];
-                j -= gab;
+                j = j - gab;
             }
             array[j + gab] = temp;
         }
+        gab = gab / 3;
         print_array(array, size);
     }
 }
